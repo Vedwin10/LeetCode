d63783f5b9be2c82919fd6629029167d5f5b8b12
Vedik Upadhyay <justvedik@gmail.com>
2025-08-19 23:37:37 -0500

    TwoSum

---
+++ TwoSum/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mp.find(target - nums[i]) != mp.end()) 
+                return {mp[target - nums[i]], i};
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
+};
+
+// Use hashmap to keep tracked of seen numbers and their indexes, and see if target - i exists in the array. 
+// If it does, return the pair.
