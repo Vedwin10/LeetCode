5d91a29420b98465f8f587f543ad37f13d461600
Vedik Upadhyay <justvedik@gmail.com>
2025-11-04 23:31:18 -0600

    optimized Valid Palindrome

--- ValidPalindrome/solution.cpp
+++ ValidPalindrome/solution.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string cleaned = "";
-        for (int i = 0; i < s.size(); ++i) {
-            if (isalnum(s[i])) cleaned += tolower(s[i]);
-        }
-
-        for (int i = 0; i < cleaned.size() / 2; ++i) {
-            if (cleaned[i] != cleaned[cleaned.size()-1-i]) return false;
-        }
-
+        string str = "";
+        for (char c : s) if (isalnum(c)) str += tolower(c);
+        for (int i = 0; i < str.size(); ++i) if (str[i] != str[str.size()-1-i]) return false;
         return true;
     }
 };
